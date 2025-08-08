2920be1354d67d1860a18c6bae408fad19d8d459
ANKIT MAHADANI <121050943+Ankit-Mahadani@users.noreply.github.com>
2025-08-08 22:06:52 +0530

    Add files via upload

---
+++ #teachersday/DAY15/boyorgirl.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    string username;
+    cin >> username;
+
+    unordered_set<char> distinctChars;
+
+    for (char c : username) {
+        distinctChars.insert(c);
+    }
+
+    if (distinctChars.size() % 2 == 0)
+        cout << "CHAT WITH HER!" << endl;
+    else
+        cout << "IGNORE HIM!" << endl;
+
+    return 0;
+}
