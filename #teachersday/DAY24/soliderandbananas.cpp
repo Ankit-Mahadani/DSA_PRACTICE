94f7851f7cfcd8349d28e4a83708325ccab00ab7
ANKIT MAHADANI <121050943+Ankit-Mahadani@users.noreply.github.com>
2025-08-17 12:07:00 +0530

    Add files via upload

---
+++ #teachersday/DAY24/soliderandbananas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int w,n, k;
+    cin >> k >> n >> w;
+    int totalCost = 0;
+    for(int i = 1; i <= w; i++) {
+        totalCost += i * k;
+    }   
+    if(totalCost > n) {
+        cout << totalCost - n << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+    return 0;
+}
