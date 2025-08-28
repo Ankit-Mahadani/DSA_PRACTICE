ff63343eec4dd2b74dbd91c56beb354f66fb12ce
ANKIT MAHADANI <121050943+Ankit-Mahadani@users.noreply.github.com>
2025-08-28 23:30:38 +0530

    Add files via upload

---
+++ #teachersday/DAY35/Panoramix.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int nextPrime = n + 1;
+    while (!isPrime(nextPrime)) {
+        nextPrime++;
+    }
+
+    if (nextPrime == m) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+
+    return 0;
+}
